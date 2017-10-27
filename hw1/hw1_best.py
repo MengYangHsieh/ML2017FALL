22ebfe7692ff873f9cae3b58c8174e907c2e4907
MengYangHsieh <r06943138@ntu.edu.tw>
2017-10-27 23:30:12 +0800

    commit hw2

--- hw1/hw1_best.py
+++ hw1/hw1_best.py
@@ -5,105 +5,105 @@ import random
 import math
 import sys
 
-# data = []
-# # 每一個維度儲存一種污染物的資訊
+data = []
+# 每一個維度儲存一種污染物的資訊
+for i in range(18):
+	data.append([])
+
+n_row = 0
+text = open('data/train.csv', 'r', encoding='big5') 
+row = csv.reader(text , delimiter=",")
+for r in row:
+	# 第0列沒有資訊
+	if n_row != 0:
+		# 每一列只有第3-27格有值(1天內24小時的數值)
+		for i in range(3,27):
+			if r[i] != "NR" and float(r[i]) != -1:
+				data[(n_row-1)%18].append(float(r[i]))
+			elif r[i] != "NR" and float(r[i]) == -1:
+				# print(r)
+				if r[i-1] !="PM2.5":
+					r[i] = r[i-1]
+				else:
+					j = i + 1
+					while float(r[j]) == -1:
+						j += 1
+					r[i] = r[j]
+				# print(r)
+				data[(n_row-1)%18].append(float(r[i]))
+			else:
+				data[(n_row-1)%18].append(float(0))
+	n_row = n_row+1
+text.close()
 # for i in range(18):
-# 	data.append([])
+	# for j in range(10):
+		# print(data[i][5759 - j])
+	# print("") 
 
-# n_row = 0
-# text = open('data/train.csv', 'r', encoding='big5') 
-# row = csv.reader(text , delimiter=",")
-# for r in row:
-# 	# 第0列沒有資訊
-# 	if n_row != 0:
-# 		# 每一列只有第3-27格有值(1天內24小時的數值)
-# 		for i in range(3,27):
-# 			if r[i] != "NR" and float(r[i]) != -1:
-# 				data[(n_row-1)%18].append(float(r[i]))
-# 			elif r[i] != "NR" and float(r[i]) == -1:
-# 				# print(r)
-# 				if r[i-1] !="PM2.5":
-# 					r[i] = r[i-1]
-# 				else:
-# 					j = i + 1
-# 					while float(r[j]) == -1:
-# 						j += 1
-# 					r[i] = r[j]
-# 				# print(r)
-# 				data[(n_row-1)%18].append(float(r[i]))
-# 			else:
-# 				data[(n_row-1)%18].append(float(0))
-# 	n_row = n_row+1
-# text.close()
-# # for i in range(18):
-# 	# for j in range(10):
-# 		# print(data[i][5759 - j])
-# 	# print("") 
+x = []
+y = []
+data_i = 0
+# 每 12 個月
+for i in range(11):
+	if i == 6:
+		data_i += 1
+	# 一個月取連續10小時的data可以有471筆
+	for j in range(471):
+		x.append([])
+		# 18種污染物
+		for t in range(18):
+			if t != 8 and t != 9 and t != 10:
+			# if t != 9 and t != 10 and t != 15 and t != 16:
+				continue
+			# 連續9小時
+			for s in range(9):
+				x[471*i+j].append(data[t][480*data_i+j+s] )
+		y.append(data[9][480*data_i+j+9])
+	data_i += 1
+x = np.array(x)
+y = np.array(y)
 
-# x = []
-# y = []
-# data_i = 0
-# # 每 12 個月
-# for i in range(11):
-# 	if i == 6:
-# 		data_i += 1
-# 	# 一個月取連續10小時的data可以有471筆
-# 	for j in range(471):
-# 		x.append([])
-# 		# 18種污染物
-# 		for t in range(18):
-# 			if t != 8 and t != 9 and t != 10:
-# 			# if t != 9 and t != 10 and t != 15 and t != 16:
-# 				continue
-# 			# 連續9小時
-# 			for s in range(9):
-# 				x[471*i+j].append(data[t][480*data_i+j+s] )
-# 		y.append(data[9][480*data_i+j+9])
-# 	data_i += 1
-# x = np.array(x)
-# y = np.array(y)
+# add cubic term
+# x = np.concatenate((x,x**3), axis=1)
 
-# # add cubic term
-# # x = np.concatenate((x,x**3), axis=1)
+# add square term
+x = np.concatenate((x,x**2), axis=1)
 
-# # add square term
-# x = np.concatenate((x,x**2), axis=1)
+# add bias
+x = np.concatenate((np.ones((x.shape[0],1)),x), axis=1)
 
-# # add bias
-# x = np.concatenate((np.ones((x.shape[0],1)),x), axis=1)
+w = np.zeros(len(x[0]))
+l_rate = 1
+repeat = 10000
 
-# w = np.zeros(len(x[0]))
-# l_rate = 1
-# repeat = 10000
+# use close form to check whether ur gradient descent is good
+# however, this cannot be used in hw1.sh 
+w_best = np.matmul(np.matmul(inv(np.matmul(x.transpose(),x)),x.transpose()),y)
 
-# # use close form to check whether ur gradient descent is good
-# # however, this cannot be used in hw1.sh 
-# w_best = np.matmul(np.matmul(inv(np.matmul(x.transpose(),x)),x.transpose()),y)
+x_t = x.transpose()
+s_gra = np.zeros(len(x[0]))
 
-# x_t = x.transpose()
-# s_gra = np.zeros(len(x[0]))
+for i in range(repeat):
+	hypo = np.dot(x,w)
+	loss = hypo - y
+	cost = np.sum(loss**2) / len(x)
+	cost_a  = math.sqrt(cost)
+	gra = np.dot(x_t,loss)
+	s_gra += gra**2
+	ada = np.sqrt(s_gra)
+	w = w - l_rate * gra/ada
+	print ('iteration: %d | Cost: %f  ' % ( i,cost_a))
 
-# for i in range(repeat):
-# 	hypo = np.dot(x,w)
-# 	loss = hypo - y
-# 	cost = np.sum(loss**2) / len(x)
-# 	cost_a  = math.sqrt(cost)
-# 	gra = np.dot(x_t,loss)
-# 	s_gra += gra**2
-# 	ada = np.sqrt(s_gra)
-# 	w = w - l_rate * gra/ada
-# 	print ('iteration: %d | Cost: %f  ' % ( i,cost_a))
-
-# w = w_best
-# # save model
-# np.save('hw1_best.npy',w)
+w = w_best
+# save model
+np.save('hw1_best.npy',w)
 # read model
 w = np.load('hw1_best.npy')
 # print(np.sum(w-w_best))
 
 test_x = []
 n_row = 0
-text = open(sys.argv[1] ,"r")
+text = open("test.csv" ,"r")
 row = csv.reader(text , delimiter= ",")
 count = 0
 
---
+++ hw2/logistic.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+
+import feature_extractor
+
+import numpy as np
+import sys
+import os.path
+import pickle
+
+feature_config = {
+    "bias": [1],
+    "age": [1, 2],
+    "fnlwgt": [],
+    "education-num": [],
+    "capital-gain": [1, 2],
+    "capital-loss": [1, 2],
+    "hours-per-week": [1, 2, 3],
+    "workclass": [0],
+    "education": [0],
+    "marital-status": [0],
+    "occupation": [0],
+    "relationship": [0],
+    "race": [0],
+    "sex": [0],
+    "native-country": [0],
+}
+is_normalized = True
+
+
+def sigmoid(z):
+    sig = 1 / (1.0 + np.exp(-z))
+    return np.clip(sig, 1e-16, 1.0 - 1e-16)
+
+
+def err(loss):
+    return np.sum(np.abs(loss))
+
+
+def cross_entropy(y_hat, y):
+    return np.sum(-(y_hat.transpose().dot(np.log(y)) +
+                    (1 - y_hat).transpose().dot(np.log(1 - y))))
+
+
+model_file_name = "./model"
+is_model_existed = os.path.isfile(model_file_name)
+
+if not is_model_existed:
+    training_x, training_y, testing_x = feature_extractor.extract_features(
+        sys.argv[1], sys.argv[2], feature_config, is_normalized)
+
+    num_validating_data = training_x.shape[0] // 10
+    validating_x = training_x[:num_validating_data]
+    validating_y = training_y[:num_validating_data]
+    training_x = training_x[num_validating_data:]
+    training_y = training_y[num_validating_data:]
+    num_training_data = training_x.shape[0]
+
+    num_features = training_x.shape[1]
+    weights = [0.0 for _ in range(num_features)]
+
+    # Training parameters
+    num_iterations = 100
+    learning_rate = 1e1
+    is_regularized = True
+    lamda = 1e2
+
+    print("========== Training ==========")
+    previous_gradient = [0.0 for _ in range(num_features)]
+    weights = np.matrix([weights], dtype=np.float64).transpose()
+    previous_gradient = np.matrix(
+        [previous_gradient], dtype=np.float64).transpose()
+    for t in range(int(num_iterations)):
+        y = sigmoid(training_x.dot(weights))
+        loss = y - training_y
+        accuracy = 1.0 - np.sum(np.abs(
+            (y + 0.5) // 1 - training_y)) / float(num_training_data)
+        gradient = training_x.transpose().dot(loss)
+        if is_regularized:
+            regularizer = lamda * np.sum(weights)
+            gradient += regularizer
+            loss += regularizer
+        previous_gradient += np.square(gradient)
+        weights -= learning_rate * gradient / np.sqrt(previous_gradient)
+        if t % 100 == 0:
+            print("ERR: %f \tL: %f \tACCURACY: %f%%" %
+                  (err(loss) / float(num_training_data),
+                   cross_entropy(training_y, y), accuracy * 100))
+
+    print("")
+    print("========== Validating ==========")
+    y = sigmoid(validating_x.dot(weights))
+    loss = y - validating_y
+    accuracy = 1.0 - np.sum(np.abs(
+        (y + 0.5) // 1 - validating_y)) / float(num_validating_data)
+    print("ERR: %f \tL: %f \tACCURACY: %f%%" %
+          (err(loss) / float(num_validating_data), cross_entropy(validating_y, y),
+           accuracy * 100))
+
+    print("")
+    print("========== Summary ==========")
+    print("Features used:")
+    for k, v in feature_config.items():
+        print(" ", k, v)
+    print("Normalization:", is_normalized)
+    print("# of iterations:", num_iterations)
+    print("Learning rate:", learning_rate)
+    print("Regularization:", is_regularized)
+    print("Lambda:", lamda)
+    print("ACCURACY: %f%%" % (accuracy * 100))
+    print("")
+
+    output_file_name = sys.argv[6]
+    with open(output_file_name, "w") as output_file:
+        output_file.write("id,label\n")
+        i = 0
+        for row in testing_x:
+            i += 1
+            y = sigmoid(row.dot(weights))
+            output_file.write(str(i) + ",")
+            output_file.write("1\n" if y >= 0.5 else "0\n")
+else:
+    s = ""
+    with open(model_file_name, "rb") as model:
+        s = pickle.load(model)
+    output_file_name = sys.argv[6]
+    with open(output_file_name, "w") as output_file:
+        output_file.write(s)
